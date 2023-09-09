0f14b85f1de2f141b9426e8c5a4478ff05c26f57
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-09-09 14:20:04 +0100

    added games

---
+++ src/pages/Game/Quiz2.tsx
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+
+const Quiz2 = () => {
+  return (
+    <div>
+      <Container>
+        <CardHold>
+          {/* <Content> */}
+          <HeaderText>Question 2</HeaderText>
+          <Question>What does CSS stand for?</Question>
+          <InputHold>
+            <Input type="radio" />
+            <Text>Central Style Sheets</Text>
+          </InputHold>
+          <InputHold>
+            <Input type="radio" />
+            <Text>Central Style Sheets</Text>
+          </InputHold>
+          <InputHold>
+            <Input type="radio" />
+            <Text>Central Style Sheets</Text>
+          </InputHold>
+          <InputHold>
+            <Input type="radio" />
+            <Text>Central Style Sheets</Text>
+          </InputHold>
+          {/* </Content> */}
+        </CardHold>
+        <ButtonHold>
+          <Button>Next</Button>
+        </ButtonHold>
+      </Container>
+    </div>
+  );
+};
+
+export default Quiz2;
+
+const Button = styled.button`
+  background-color: #0d71fa;
+  color: white;
+  width: 100px;
+  height: 50px;
+`;
+
+const ButtonHold = styled.div`
+  width: 90%;
+  height: 100px;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const Text = styled.div`
+  margin-left: 10px;
+`;
+
+const Input = styled.input`
+  width: 20px;
+  height: 20px;
+`;
+
+const InputHold = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  font-weight: 500;
+  font-size: 18px;
+`;
+
+const Question = styled.div`
+  font-weight: 800;
+  font-size: 25px;
+  margin-bottom: 20px;
+`;
+
+const HeaderText = styled.div`
+  font-weight: bold;
+  font-size: 45px;
+  font-style: italic;
+  margin-bottom: 30px;
+`;
+
+// const Content = styled.div``;
+
+const CardHold = styled.div`
+  width: 90%;
+  height: 60vh;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  padding: 30px;
+  flex-direction: column;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #fef0ef;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
