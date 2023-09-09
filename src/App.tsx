40475861759700866ac0443e9beb9adaae672312
Damilola Fortune Sanni <nicsylvia15f@gmail.com>
2023-09-09 13:55:32 +0100

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Suspense } from "react";
-import { MajorRoute } from "./router/MajorRoute";
-import LoadingPage from "./pages/Desktop/LoadingPage/LoadingPage";
+
 
 const App = () => {
   return (
     <div>
-      <Suspense fallback={<LoadingPage />}>
-        <MajorRoute />
-      </Suspense>
+     
+        < />
+     
     </div>
   );
 };
--- src/router/desktopRouter.tsx
+++ src/router/desktopRouter.tsx
@@ -1,12 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import { lazy } from "react";
 import DesktopLayout from "../components/Layout/DesktopLayoutLandingPage/DesktopLayout";
-
-const LandingPage = lazy(
-  () => import("../pages/Desktop/LandingPage/LandingPage")
-);
-// const SignUp = lazy(() => import("../pages/Desktop/AuthPages/SignUp"));
-// const Login = lazy(() => import("../pages/Desktop/AuthPages/Login"));
+import LandingPage from "../pages/LandingPage/LandingPage";
+import MyCourse from "../pages/ViewCourses/MyCourse";
+import Courses from "../pages/Courses/Courses";
 
 export const desktopRouter = createBrowserRouter([
   {
@@ -17,133 +13,22 @@ export const desktopRouter = createBrowserRouter([
         index: true,
         element: <LandingPage />,
       },
-    ],
-  },
-  {
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-  {
-    path: "/log-in",
-    element: <Login />,
-  },
-  {
-    path: "/otp_vertification",
-    element: <OTP />,
-  },
-  {
-    path: "/forget_password",
-    element: <UserForgetPassword />,
-  },
-  {
-    path: "/userdashboard",
-    element: <UserDashBoardLayout />,
-    children: [
       {
-        index: true,
-        element: <UserBodyDashboard />,
+        path: "/view-courses",
+        element: <MyCourse />,
       },
       {
-        path: "/userdashboard/main-investment",
-        element: <MainPage />,
+        path: "/courses",
+        element: <Courses />,
       },
       {
-        path: "/userdashboard/invest-now",
-        element: <InvestNow />,
+        path: "/auth",
+        element: <MyCourse />,
       },
       {
-        path: "/userdashboard/withdraw",
-        element: <Withdraw />,
-      },
-      {
-        path: "/userdashboard/investsuccess",
-        element: <InvestSucces />,
-      },
-      {
-        path: "/userdashboard/business-registration",
-        element: <MainBusinessRegistrationPage />,
-      },
-      {
-        path: "/userdashboard/settings",
-        element: <MainSettings />,
-      },
-      {
-        path: "/userdashboard/business-registration-progress",
-        element: <BusinessRegistrationProgess />,
-      },
-      {
-        path: "/userdashboard/bills-payment",
-        element: <BillPayment />,
-      },
-      {
-        path: "/userdashboard/bills-payment/airtime",
-        element: <Airtime />,
-      },
-      {
-        path: "/userdashboard/bills-payment/airtime-confirmation",
-        element: (
-          <div>
-            <AirtimeConfirmPage />
-            <ToastContainer
-              position="top-right"
-              autoClose={3000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="colored"
-            />
-          </div>
-        ),
-      },
-      {
-        path: "/userdashboard/bills-payment/airtime-successful",
-        element: <AirtimeSuccess />,
-      },
-      {
-        path: "/userdashboard/bills-payment/buy-data",
-        element: <BuyData />,
-      },
-      {
-        path: "/userdashboard/bills-payment/buy-confirmation",
-        element: <BuyDataConfirm />,
-      },
-      {
-        path: "/userdashboard/bills-payment/buydata-successful",
-        element: <BuyDataSuccess />,
-      },
-      {
-        path: "/userdashboard/bills-payment/cable-subscriptions",
-        element: <CableSub />,
-      },
-      {
-        path: "/userdashboard/bills-payment/cable-select",
-        element: <SelectCable />,
-      },
-      {
-        path: "/userdashboard/bills-payment/cable-confirmation",
-        element: <CableConfirmPage />,
-      },
-      {
-        path: "/userdashboard/bills-payment/cable-success",
-        element: <BuyCableSuccess />,
-      },
-      {
-        path: "/userdashboard/bills-payment/electricity",
-        element: <Electricity />,
-      },
-      {
-        path: "/userdashboard/bills-payment/electricity-select",
-        element: <SelectElectricity />,
+        path: "/view-courses",
+        element: <MyCourse />,
       },
     ],
-  },
-
-  {
-    path: "*",
-    element: <Error />,
   },
 ]);
